c2213babce8d5b462d9a2d379a7aa28a976ad0f5
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-04-19 00:23:22 -0700

    Update demo to showcase color shift

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,10 +1,40 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+import { useState } from 'react'
 import Chameleon from 'react-chameleon-theme-color'
+import Helmet from 'react-helmet'
+import useInterval from '@use-it/interval'
 
 const App = () => {
+  const randColor = () => "#xxxxxx".replace(/x/g, y => (Math.random() * 16 | 0).toString(16))
+  const [color, setColor] = useState(false)
+  const [background, setBackground] = useState(false)
+  useInterval(() => {
+    setColor(randColor())
+    setBackground(randColor())
+  }, 1000)
+  const full = css`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: ${background};
+  `
+  const inner = css`
+    background: ${color};
+    height: 500px;
+    width: 100%;
+  `
   return (
-    <div style={{ background: '#123'}}>
-      <Chameleon />
+    <div css={full}>
+      <Helmet
+        title={'react-chameleon-theme-color'}
+        htmlAttributes={{ lang: 'en' }}>
+      </Helmet>
+      <div css={inner}>
+        <Chameleon />
+      </div>
     </div>
   )
 }
