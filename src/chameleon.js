f68b39016b00effd20f703a491e576f41d73a28f
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-04-18 22:42:07 -0700

    Simplify regex

--- src/chameleon.js
+++ src/chameleon.js
@@ -18,10 +18,10 @@ const Chameleon = () => {
       return false
     }
     const trim = (value) => {
-      // Regex to match HTML colors
-      // https://gist.github.com/olmokramer/82ccce673f86db7cda5e#gistcomment-2029233
+      // Regex to match RGB and RGBA colors
+      // https://stackoverflow.com/a/27622101/9129020
       if (!value) return false
-      return value.match(/(#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}|(?:rgba?|hsla?)\((?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s\/]*[\d\.]+%?\))/i)[0]
+      return value.match(/(.*?)(rgb|rgba)\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)/i)[0]
     }
     let cur = container.current
     while (cur && trim(getProperty(cur, key)) === 'rgba(0, 0, 0, 0)') {
