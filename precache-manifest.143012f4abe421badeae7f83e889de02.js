214da637d28ffae241589d8ed634cc9e1e05f11d
root <woofers1tt@gmail.com>
2019-07-10 05:42:14 +0000

    Updates

---
+++ precache-manifest.143012f4abe421badeae7f83e889de02.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b752766bfb8943c278720d37ca82da",
+    "url": "/react-chameleon-theme-color/index.html"
+  },
+  {
+    "revision": "b90473a69d6d187754ed",
+    "url": "/react-chameleon-theme-color/static/js/2.27c52930.chunk.js"
+  },
+  {
+    "revision": "fbd61b62ea3c31e01ff6",
+    "url": "/react-chameleon-theme-color/static/js/main.78685950.chunk.js"
+  },
+  {
+    "revision": "469b142d7033223932be",
+    "url": "/react-chameleon-theme-color/static/js/runtime~main.9807ad77.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-chameleon-theme-color/precache-manifest.90b0f31781d258eeb9462114b308f0a6.js"
+  "/react-chameleon-theme-color/precache-manifest.143012f4abe421badeae7f83e889de02.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-chameleon-theme-color/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-chameleon-theme-color/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
